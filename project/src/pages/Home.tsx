02fc4b705c249c8e9cbd623670614248b1466937
Raiyan Wasi Siddiky <raiyanwasisiddiky@gmail.com>
2025-07-28 16:41:23 +0600

    light dark mode

--- project/src/pages/Home.tsx
+++ project/src/pages/Home.tsx
@@ -1,10 +1,13 @@
+import { ThemeToggle } from '../components/ThemeToggle';
+
+
 export const Home = () => {
     return (
         <>
-        <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-200"> 
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden"> 
 
             {/* Theme Toggle */} 
-
+            <ThemeToggle />
 
 
             {/* Background Effects*/}
