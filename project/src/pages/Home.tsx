c89dc2669bc512e0e715de3f8e711de2bd705423
Raiyan Wasi Siddiky <raiyanwasisiddiky@gmail.com>
2025-07-28 15:21:51 +0600

    basic setup + css

---
+++ project/src/pages/Home.tsx
@@ -0,0 +1,27 @@
+export const Home = () => {
+    return (
+        <>
+        <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-200"> 
+
+            {/* Theme Toggle */} 
+
+
+
+            {/* Background Effects*/}
+
+
+            {/* Navbar */}
+
+
+            {/* Main Content */}
+
+
+
+            {/* Footer */}
+
+        </div>
+        
+        </>
+    );
+}
+
